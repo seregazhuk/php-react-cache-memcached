2b40dd2704411921ce72b096c58b4e7403bf9235
Zhuk Sergey <seregazhuk88@gmail.com>
2018-09-15 17:34:19 +0300

    Mark class as final

--- src/Memcached.php
+++ src/Memcached.php
@@ -5,27 +5,15 @@ namespace seregazhuk\React\Cache\Memcached;
 use React\Cache\CacheInterface;
 use React\EventLoop\LoopInterface;
 use React\Promise\PromiseInterface;
-use seregazhuk\React\Memcached\Client;
 use seregazhuk\React\Memcached\Factory as ClientFactory;
 
-class Memcached implements CacheInterface
+final class Memcached implements CacheInterface
 {
-    /**
-     * @var Client
-     */
-    protected $client;
+    private $client;
 
-    /**
-     * @var string
-     */
-    protected $prefix;
+    private $prefix;
 
-    /**
-     * @param LoopInterface $loop
-     * @param string $address
-     * @param string $prefix
-     */
-    public function __construct(LoopInterface $loop, $address = 'localhost:11211', $prefix = 'react:cache:')
+    public function __construct(LoopInterface $loop, string $address = 'localhost:11211', string $prefix = 'react:cache:')
     {
         $this->client = ClientFactory::createClient($loop, $address);
         $this->prefix = $prefix;
